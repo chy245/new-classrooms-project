5e76132aea3ecbc019a5d1dc0f79984cd4b21a13
Chang Hong Yang <changhong.yang@nyu.edu>
2018-07-17 20:44:35 -0400

    add body-parser and test rest api methods

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const http = require('http')
 const path = require('path');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -15,3 +17,15 @@ app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('running'));
+
+app.route('/api/get_test').get((req, res) => {
+  console.log('got a get request');
+  console.log(req.body);
+  res.send("GET received, response returned");
+});
+
+app.route('/api/post_test').post((req, res) => {
+  console.log('got a post request');
+  console.log(req.body);
+  res.send("POST received, response returned");
+});
